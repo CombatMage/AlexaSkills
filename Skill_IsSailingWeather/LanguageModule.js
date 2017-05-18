271f7bd92e9bfeb8fbbe899b3781245b207990a9
Eric Neidhardt <eric.neidhardt@gmail.com>
2017-05-18 22:02:45 +0200

    cleanup

--- Skill_IsSailingWeather/LanguageModule.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getPositiveResponseForWindSpeed(speed) {
-    return `Die aktuelle Windstärke beträgt ${speed}`;
-}
-exports.getPositiveResponseForWindSpeed = getPositiveResponseForWindSpeed;
-//# sourceMappingURL=LanguageModule.js.map
